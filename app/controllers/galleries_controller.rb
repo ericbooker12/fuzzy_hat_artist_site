ad68b0543da4d5b91a6ebf1f025a80ccfce92b79
shawn <shawn@naturalprofessional.com>
2017-03-13 17:43:13 -0700

    Add gallery index view and controller

---
+++ app/controllers/galleries_controller.rb
@@ -0,0 +1,30 @@
+class GroupsController < ApplicationController
+  before_action :set_gallery, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @galleries = Gallery.active
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_gallery
+      @gallery = Gallery.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def gallery_params
+      params.require(:gallery).permit(:name, :archive)
+    end
+end
