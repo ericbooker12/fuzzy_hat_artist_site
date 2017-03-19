f1baa1ea3dd036a61e8829023d5c1735ec900254
Eric Booker <ericbooker12@gmail.com>
2017-03-19 10:36:43 -0700

    Add create gallery tests for user profile page

--- features/profile_page.feature
+++ features/profile_page.feature
@@ -1,24 +1,35 @@
 
 Feature: Profile Page
 	As an admin user
-	I want to go to the profile page
-	So that I add, edit and create new galleries
+	I want to have an admin profile page
+	So that I add, edit and create new galleries and collections
 
 Background: Creating a user
   Given I am on the user log in page
   And a user
   And I fill in email with "e@b.com"
   And I fill in Password with "123456"
-  When I click on "Log in"
+  When I click on "Log in" button
   Then I am on the user profile page
 
-Scenario: Adding a gallery
-	Given "Add Gallery" Button exists
-	When I press "Add New Gallery"
+Scenario: Folowing add new gallery link
+	When I click on "Add new gallery"
 	And show me the page
-	Then I should see the edit gallery page
+	Then I should see the add gallery page
+
+Scenario: Adding a new gallery
+	Given I am on the create gallery page
+	And I fill in name with "Coffee Cups"
+	When I click on "Create Gallery" button
+	Then I should see the user profile
+	And I should see "Coffee Cups"
 
 
+Scenario: Creating a collection
+	Given I am on the user profile page
+	And a gallery exists
+	When I click on the first link to add new Collection
+	Then I should see add collection page
 
 
 
--- features/step_definitions/editing_user_profile_page_steps.rb
+++ features/step_definitions/editing_user_profile_page_steps.rb
@@ -13,7 +13,7 @@ Given(/^I fill in Password with "([^"]*)"$/) do |password|
   fill_in "Password", with: password
 end
 
-When(/^I click on "([^"]*)"$/) do |button|
+When(/^I click on "([^"]*)" button$/) do |button|
   click_button button
 end
 
@@ -21,18 +21,55 @@ Then(/^I am on the user profile page$/) do
 	expect(body).to include("User Profile - Bingo")
 end
 
-Given(/^"([^"]*)" Button exists$/) do |add_gallery|
-
-end
-
-When(/^I press "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I click on "([^"]*)"$/) do |add_new_gallery|
+  click_link add_new_gallery
+  # save_and_open_page
 end
 
 When(/^show me the page$/) do
+  # save_and_open_page
+end
+
+Then(/^I should see the add gallery page$/) do
+  expect(body).to include("New Gallery")
+end
+
+Given(/^I am on the create gallery page$/) do
+  # expect(body).to include("User Profile - Bingo")
+  click_link "Add new gallery"
+  # save_and_open_page
+end
+
+# Given(/^I fill in name with "([^"]*)"$/) do |gallery_name|
+# 	fill_in "gallery[name]", with: gallery_name
+
+# 	save_and_open_page
+
+# end
+
+When(/^I fill in name with "([^"]*)" and click on "([^"]*)"$/) do |gallery_name, button|
+  fill_in "gallery[name]", with: gallery_name
+  click_link button
+end
+
+Then(/^I should see the user profile$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then(/^I should see the edit gallery page$/) do
+Then(/^I should see "([^"]*)"$/) do |arg1|
   pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^a gallery exists$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click on the first link to add new Collection$/) do
+	# all('a')[0].click
+	# page.find(:css, '.add_new_collection')[0]
+	save_and_open_page
+end
+
+Then(/^I should see add collection page$/) do
+  # expect(body).to include ("<h1>New Collection</h1>")
 end
