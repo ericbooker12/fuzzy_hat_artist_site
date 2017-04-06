4c683323a3473549f5b6ed0d901145fc5710c4f6
shawn <shawn@naturalprofessional.com>
2017-04-05 19:30:50 -0700

    Add styling so images center in carousel

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@
   belongs_to :collection
 
   has_attached_file :image,
-            styles: { large: "1140x600", medium: "800x600>", thumb: "200x200#" },
+            styles: { large: "600x870>", medium: "800x600>", thumb: "200x200#" },
             default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
