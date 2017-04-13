43dcb095c4d273817b49efcde0266a4991013651
shawn <shawn@naturalprofessional.com>
2017-04-12 17:58:57 -0700

    Ready Seed file for seeding production. Still need S3 bucket names

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,50 +3,64 @@ Collection.delete_all
 Gallery.delete_all
 User.delete_all
 
-# ga = Gallery.new(name: "gall")
-user = User.new(email: "be@bop.com", password: "123456")
+def create_artist(email, password)
+	user = User.new(email: email, password: password)
+	user.save
+	user
+end
 
-mediums = ["Ceramics", "Photography"]
-
-mediums.each do |medium|
-	gallery = user.galleries.build(name: medium)
+def create_gallery(gallery_name, user)
+	gallery = user.galleries.build(name: gallery_name)
 	gallery.save
 end
 
-3.times do |i|
-	gallery = Gallery.first
-	collection = gallery.collections.build(name: "Collection#{i+1}", thumbnail: 1)
+def create_collection(collection_name, gallery_name)
+	gallery = Gallery.where(name: gallery_name).first
+	collection = gallery.collections.build(name: collection_name, thumbnail: 1)
 	collection.save
+end
 
-	10.times do |j|
-		item = collection.items.build(title: "Title#{j+1}")
+def add_items(collection_name, images_folder)
+	collection = Collection.where(name: collection_name).first
+	# images = Dir.glob("#{Rails.root}/public/images/glass/*.jpg")
+	images = Dir.glob(File.join("#{Rails.root}", images_folder))
+
+	i = 1
+	images.each do |photo_image|
+		item = collection.items.build(title: "Title #{i}")
+		item.image = File.new(photo_image)
 		item.save
+		i += 1
 	end
-	collection.thumbnail = collection.items.first.id
+end
+
+def assign_thumbnail(collection_name)
+	collection = Collection.where(name: collection_name)
+	collection[:thumbnail] = collection.items.first.id
 	collection.save
 end
 
-gallery = user.galleries.build(name: "Glass")
-gallery.save
+user = create_artist('be@bop.com', '123456')
 
-collection = gallery.collections.build(name: "Primordial Roots", thumbnail: 1)
-collection.save
+create_gallery('Glass', user)
+create_gallery('Ceramic', user)
+create_gallery('Photography', user)
 
-images = Dir.glob("#{Rails.root}/public/images/glass/*.jpg")
-# images = Dir.glob(File.join(Rails.root, '/public/images/glass/*.jpg')
+create_collection('Primordial Roots', 'Glass')
+create_collection('River Rocks', 'Glass')
+create_collection('Borne from the Earth', 'Ceramic')
+create_collection('Light & Shadow', 'Photography')
+create_collection('Somthing else @@@@@@@@@@@@', 'Photography')
 
-i = 1
-images.each do |photo_image|
-	item = collection.items.build(title: "Title #{i}")
-	item.image = File.new(photo_image)
-	item.save
-	i += 1
-end
+# http://docs.aws.amazon.com/AWSRubySDK/latest/AWS/S3/ObjectCollection.html
+add_items('Primordial Roots', '/public/images/glass/*.jpg')
+add_items('River Rocks', '/public/images/glass/*.jpg')
+add_items('Borne from the Earth', '/public/images/glass/*.jpg')
+add_items('Light & Shadow', '/public/images/glass/*.jpg')
+add_items('Somthing else @@@@@@@@@@@@', '/public/images/glass/*.jpg')
 
-collection[:thumbnail] = collection.items.first.id
-collection.save
-
-
-
-# image: File.new("image/to/path.jpg")
-# :photo => File.open(File.join(Rails.root, '/public/images/DSCF9692.jpg'))
+assign_thumbnail('Primordial Roots')
+assign_thumbnail('River Rocks')
+assign_thumbnail('Borne from the Earth')
+assign_thumbnail('Light & Shadow')
+assign_thumbnail('Somthing else @@@@@@@@@@@@')
