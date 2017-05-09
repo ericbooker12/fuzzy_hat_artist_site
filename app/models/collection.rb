2fab933e9beabc9603f9182958f2d240e8970c80
shawn <shawn@naturalprofessional.com>
2017-05-09 13:22:21 -0700

    Unit tests for Collection Model

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,9 +1,9 @@
 class Collection < ApplicationRecord
   belongs_to :gallery
   has_many :items, dependent: :restrict_with_error
-	validates :name, presence: true
-	validates :thumbnail, presence: true
-	validates :name, uniqueness: true
+	validates :name, presence: true, uniqueness: true
+	validates :thumbnail, presence: true, numericality: { only_integer: true }
+	# validates :name, uniqueness: true
 
   def self.active
     Collection.where(archive: false)
