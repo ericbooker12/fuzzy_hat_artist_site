f4e79cd2fa2357d57a0c196df96b3e259e80d640
shawn <shawn@naturalprofessional.com>
2017-05-11 10:52:05 -0700

    Add valid test for item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    item = create(:item)
+    expect(item).to be_valid
+  end
+
 end
