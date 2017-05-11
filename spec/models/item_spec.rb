f3963ef00fb6594bcd6acdfd7635ffb87a7b6e2c
shawn <shawn@naturalprofessional.com>
2017-05-11 11:04:25 -0700

    Add valid test for item. Add paperclip tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe Item, type: :model do
     expect(item).to be_valid
   end
 
+  describe Item do
+    it { should have_attached_file(:image) }
+    it { should validate_attachment_presence(:image) }
+    it { should validate_attachment_content_type(:image).
+                  allowing('image/png', 'image/gif').
+                  rejecting('text/plain', 'text/xml') }
+    it { should validate_attachment_size(:image).
+                  less_than(2.megabytes) }
+  end
+
 end
