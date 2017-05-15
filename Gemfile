bd50666656e9f7c1ac50992047e17ac31f34c848
shawn <shawn@naturalprofessional.com>
2017-05-14 22:33:29 -0700

    Add rails_controller_testing gem to test render methods in controller tests. Add render items#index view test

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   gem 'byebug', platform: :mri
   gem 'rspec-rails', '~> 3.5', '>= 3.5.2'
   gem 'factory_girl_rails', '~> 4.8'
+  gem 'rails-controller-testing', '~> 1.0', '>= 1.0.1'
 end
 
 group :development do
--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,38 +1,48 @@
 require 'rails_helper'
-# require 'spec_helper'
 
 RSpec.describe ItemsController, type: :controller do
-  it { should use_before_action(:set_item) }
-  it { should use_before_action(:set_collection) }
-  it { should use_before_action(:authenticate_user!) }
+    it { should use_before_action(:set_item) }
+    it { should use_before_action(:set_collection) }
+    it { should use_before_action(:authenticate_user!) }
 
-  it do
-    user = create(:user)
-    sign_in(user)
+  describe "actions requiring being logged in" do
+    it do
+      user = create(:user)
+      sign_in(user)
 
-    sample_image = fixture_file_upload((Rails.root.join('spec', 'photos', 'test.png')))
-    collection = create(:collection)
+      sample_image = fixture_file_upload((Rails.root.join('spec', 'photos', 'test.png')))
+      collection = create(:collection)
 
-    params = {
-      item: {
-        title: "Artwork",
-        image: sample_image,
-        width: 6,
-        length: 6,
-        height: 8 },
-        collection_id: collection.id
-    }
-    should permit(:title, :image, :width, :length, :height, :collection_id).
-    for(:create, params: params).
-      on(:item)
+      params = {
+        item: {
+          title: "Artwork",
+          image: sample_image,
+          width: 6,
+          length: 6,
+          height: 8 },
+          collection_id: collection.id
+      }
+      should permit(:title, :image, :width, :length, :height, :collection_id).
+      for(:create, params: { params: params } ).
+        on(:item)
+    end
   end
 
-  describe "GET #show_new_item_form" do
-    it "returns http success"
-    # do
-    #   get :show_new_item_form
-    #   expect(response).to have_http_status(:success)
-    # end
+  describe "actions requiring not being logged in" do
+    it do
+      collection = create(:collection)
+      get :index, params: { collection_id: collection.id }
+
+      should render_template('index')
+    end
+  # describe "GET #show_new_item_form" do
+  #   it "returns http success"
+  #   # do
+  #   #   get :show_new_item_form
+  #   #   expect(response).to have_http_status(:success)
+  #   # end
+  # end
+
   end
 
 end
