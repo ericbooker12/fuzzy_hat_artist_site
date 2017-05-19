2d885c8b2aed8dbf51733764242dc1578224bbe2
shawn <shawn@naturalprofessional.com>
2017-05-18 18:23:32 -0700

    Add archive action to the before_action set_gallery method

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,5 +1,5 @@
 class GalleriesController < ApplicationController
-  before_action :set_gallery, only: [:show, :edit, :update, :destroy]
+  before_action :set_gallery, only: [:show, :edit, :update, :destroy, :archive]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
