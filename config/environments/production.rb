03f1967d7a8a5ce26d0193c8d1729ad1091aafb6
shawn <shawn@naturalprofessional.com>
2017-05-22 19:26:37 -0700

    Change paperclip cdn different than asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
     storage: :s3,
     url: ":s3_alias_url",
     path: "/:class/:attachment/:id_partition/:style/:filename",
-    s3_host_alias: "d2cobm7dsc46xl.cloudfront.net",
+    s3_host_alias: "d1vbh98hzc4ro6.cloudfront.net",
     s3_credentials: {
         bucket: ENV['S3_BUCKET'],
         access_key_id: ENV['AWS_ACCESS_KEY_ID'],
