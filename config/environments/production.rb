f8f5beed36e376a0c1cc5b25b0e810920c6308a1
shawn <shawn@naturalprofessional.com>
2017-09-03 17:40:15 -0600

    Troubleshooting glyphicons. Add Access-Control-Allow-Origin to production.rb And try material icons

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
 
   config.public_file_server.headers = {
-    'Cache-Control' => 'public, max-age=31536000'
+    'Cache-Control' => 'public, max-age=31536000',
     'Access-Control-Allow-Origin' => '*'
   }
   config.assets.js_compressor = :uglifier
